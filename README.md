39cc036dee11196655b2193dcd4b4f3122c2b623
doudou <edouard.comtet@gmail.com>
2017-11-07 23:18:13 +0100

    Improve conf and prepare release

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-test
+# Simplis
+
+## Release
+
+Run `npm run build` and change `Ajouter une vente.html`, line 98, load `style.css` and not `bundle.js`.
+Change in webpack conf the conf to extract CSS.
+
+## Build
+
+You can build it with `npm run serve` and try it.
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractCSS = new ExtractTextPlugin({ filename: "style.css" });
+
 module.exports = {
     watch: true,
     entry: path.resolve('./src/my.js'),
@@ -17,18 +20,14 @@ module.exports = {
         inline: true,
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        extractCSS
     ],
     module: {
         rules: [{
             test: /\.scss$/,
-            use: [{
-                loader: "style-loader"
-            }, {
-                loader: "css-loader"
-            }, {
-                loader: "sass-loader"
-            }]
+            use: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "sass-loader" }]
+            //use: extractCSS.extract({ use: [ { loader: "css-loader" }, { loader: "sass-loader" } ] })
         }]
     }
   };
